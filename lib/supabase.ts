d39a748c7de0cbc1d059fa94cef4e99f831e70a1
Dhairya Gandhi <dgandhi@umd.edu>
2026-04-23 17:57:37 -0500

    api keys added

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,22 @@
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from '@/constants/config';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from 'expo-secure-store';
 import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+    getItem: (key: string) => {
+        return SecureStore.getItemAsync(key);
+    },
+    setItem: (key: string, value: string) => {
+        SecureStore.setItemAsync(key, value);
+    },
+    removeItem: (key: string) => {
+        SecureStore.deleteItemAsync(key);
+    },
+};
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     auth: {
-        storage: AsyncStorage,
+        storage: ExpoSecureStoreAdapter as any,
         autoRefreshToken: true,
         persistSession: true,
         detectSessionInUrl: false,
